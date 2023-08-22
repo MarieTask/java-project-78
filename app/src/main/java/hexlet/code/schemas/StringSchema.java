f2ca4ad45eaa9078a1f7a96046c34f1cbe8aaa3a
Мария Таскина <maryyysh2016@mail.ru>
2023-08-22 15:58:47 +0300

    step 2

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,27 @@
+package hexlet.code.schemas;
+
+import hexlet.code.MinLength;
+import hexlet.code.NotNull;
+import hexlet.code.Value;
+
+public class StringSchema {
+
+    public void string() {
+
+    }
+
+    @NotNull
+    public void required() {
+
+    }
+
+    @MinLength
+    public void minLength() {
+
+    }
+
+    @Value
+    public void contains() {
+
+    }
+}
