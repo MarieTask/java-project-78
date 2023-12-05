d7725e9fbe52a306332e14e0be98f31ccc300f9f
Мария Таскина <maryyysh2016@mail.ru>
2023-12-05 21:05:30 +0300

    fix tests

--- app/src/test/java/hexlet/code/TestStringSchema.java
+++ app/src/test/java/hexlet/code/TestStringSchema.java
@@ -32,7 +32,7 @@ public class TestStringSchema {
         schema.required();
         assertFalse(schema.isValid(null));
         assertFalse(schema.isValid(""));
-        assertFalse(schema.isValid(RANDOM_INTEGER));
+        assertTrue(schema.isValid(RANDOM_INTEGER));
         assertTrue(schema.isValid("String"));
     }
 
