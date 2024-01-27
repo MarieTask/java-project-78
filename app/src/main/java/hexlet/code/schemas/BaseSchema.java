3a4b7b9a702a68b7a89514853bc45e95015fee0e
Мария Таскина <maryyysh2016@mail.ru>
2024-01-27 21:36:05 +0300

    try to fix tests rev.8

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -20,7 +20,7 @@ public abstract class BaseSchema {
         conditions.add(condition);
     }
     public final boolean isValid(Object obj) {
-        if (!isRequired && conditions.stream().findFirst().equals(false)) {
+        if (!isRequired && !conditions.stream().findFirst().equals(false)) {
             return true;
         }
         return (conditions.stream().allMatch(condition -> condition.test(obj)));
